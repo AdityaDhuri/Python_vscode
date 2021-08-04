32f372c3bda209c15850b5ba9db77e527a081192
AdityaDhuri <87560794+AdityaDhuri@users.noreply.github.com>
2021-08-04 19:36:39 +0530

    Update 01_hello.py

--- 01_hello.py
+++ 01_hello.py
@@ -1,11 +1,1 @@
-'''
 print("Hello world")
-print(''nknkmkmkmkm
-bhjbajbjibaucij
-lkkxisncssxs'')
-
-nhi ayega 
-'''
-a=int(input("enter a 1st no "))
-b=int(input("enter a 2nd no "))
-print("the avg of a & b is ", (a+b)/2)
