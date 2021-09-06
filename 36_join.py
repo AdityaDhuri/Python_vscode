086bf4bec45098ea32366816b863bee2393ce886
Aditya S Dhuri <87560794+AdityaDhuri@users.noreply.github.com>
2021-09-06 22:15:21 +0530

    Add files via upload

---
+++ 36_join.py
@@ -0,0 +1,5 @@
+l = ["camera", "aditya", "mumma", "flower"]
+sentence = " and ".join(l)  #this will join the words inside l with and
+print(sentence)
+b = "\n".join(l)   #this will print it vertically contents inside list must be string
+print(b)
