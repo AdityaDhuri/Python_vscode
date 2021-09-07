34ee46a844239613c1e1964b23d2786befdba015
Aditya S Dhuri <87560794+AdityaDhuri@users.noreply.github.com>
2021-09-07 22:21:31 +0530

    Add files via upload

---
+++ 37_format.py
@@ -0,0 +1,7 @@
+name = "Aditya"
+surname = "Dhuri"
+study = "Engineering"
+a = "my name is {} and my surname is {}. I am a {} student".format(name, surname, study)
+b = "my name is {2} and my surname is {1}. I am a {0} student".format(name, surname, study)
+print(a)
+print(b)
